13b0e26d57a370f4d6f621ae5c561ab80b2543d8
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-04-01 18:05:33 -0300

    adc icones

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,68 +8,94 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final textFieldNomeUsuario = TextEditingController();
+  final textFieldEmailUsuario = TextEditingController();
   final textFieldSenhaUsuario = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final emailField = TextField(
+      autofocus: true,
+      keyboardType: TextInputType.text,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+      ),
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.person,
+        ),
+        hintText: "Email",
+        labelStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+      controller: textFieldEmailUsuario,
+    );
+
+    final senhaField = TextField(
+      autofocus: true,
+      obscureText: true,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.lock_outline),
+        hintText: "Password",
+        labelStyle: TextStyle(color: Colors.black),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+      controller: textFieldSenhaUsuario,
+    );
+
+    final buttonLogin = ButtonTheme(
+      height: 40.0,
+      child: RaisedButton(
+        onPressed: () => {print("usuario logado")},
+        shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(20.0)),
+        child: Text(
+          "Login",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-          DrawerHeader(
-            child: Text('Troca de Livros'),
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-            ),
-          ),
-        ]),
-      ),
-      appBar: AppBar(
-        title: Text("Troca de Livros"),
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(0),
+          color: Colors.white,
+          padding: const EdgeInsets.all(40),
+          child: Column(
             children: <Widget>[
-              TextField(
-                autofocus: true,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: "Nome do usuário",
-                  labelStyle: TextStyle(color: Colors.black),
-                ),
-                controller: textFieldNomeUsuario,
+              SizedBox(
+                height: 130,
+                child: Image.asset("imagens/person.png", fit: BoxFit.contain),
               ),
-              TextField(
-                autofocus: true,
-                obscureText: true,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: "Senha do usuário",
-                  labelStyle: TextStyle(color: Colors.black),
-                ),
-                controller: textFieldSenhaUsuario,
-              ),
-            ]),
+              SizedBox(height: 40),
+              emailField,
+              SizedBox(height: 20),
+              senhaField,
+              SizedBox(height: 30),
+              buttonLogin,
+            ],
+          ),
+        ),
       ),
-      backgroundColor: Colors.lightBlue[50],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print(textFieldNomeUsuario.text);
-          print(textFieldSenhaUsuario.text);
-          return showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                content: Text("Usuario cadastrado com sucesso!"),
-              );
-            },
-          );
-        },
-        child: Icon(Icons.check),
-      ),
+      backgroundColor: Colors.white,
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     print(textFieldEmailUsuario.text);
+      //     print(textFieldSenhaUsuario.text);
+      //     return showDialog(
+      //       context: context,
+      //       builder: (context) {
+      //         return AlertDialog(
+      //           content: Text("Usuario cadastrado com sucesso!"),
+      //         );
+      //       },
+      //     );
+      //   },
+      //   child: Icon(Icons.check),
+      // ),
     );
   }
 }
